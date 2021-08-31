cf154f97f4215737f342d3dfb52701981116584f
justcabyr <asulaiman@dervac.com>
2021-08-31 06:38:09 +0100

    Setup opn

---
+++ url.js
@@ -0,0 +1,10 @@
+const opn = require('opn')
+
+// setTimeout(function() {
+//     opn('https://www.github.com/JustCabyr') 
+//   }, 3000);
+
+setInterval(function() {
+    // your code goes here...
+    opn('https://www.github.com/JustCabyr') 
+}, 5 * 1000); // 60 * 1000 milsec
